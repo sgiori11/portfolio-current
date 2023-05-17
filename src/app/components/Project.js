b27252024031213776dc12db354465876c43e9be
“sgiori11” <stephaniegiori9@gmail.com>
2023-05-17 21:55:20 +0200

    json jobs file + Project component

---
+++ src/app/components/Project.js
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '../styles/Project.module.css';
+
+export default function Project() {
+    return (
+        <h1>hello bish im a project div</h1>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import HeroSection from './components/HeroSection';
 import Sidebar from './components/Sidebar';
+import Project from './components/Project';
 
 export default function Home() {
   return (
@@ -9,11 +10,12 @@ export default function Home() {
    <HeroSection />
    <section className={styles.pageContent}>
     <Sidebar className={styles.sidebar} />
-    <h2 className={styles.projects}>projects
-      <span style={{fontSize: "10px", fontWeight: "200"}}>In this example, justify-content: flex-end; in the .pageContent class will align the sidebar (child elements) to the right side. The width: 100%; ensures that the .pageContent section spans the full width of the viewport.
 
-This solution assumes that your layout allows the pageContent to stretch the full width of the viewport. If there are other constraints in your layout preventing this, you may need a different solution. If that's the case, please provide more context and I'll be happy to help further.</span>
-    </h2>
+    <section className={styles.projects}>
+      <h2>projects</h2>
+      <Project />
+    </section>
+    
     <h2 className={styles.skills}>Skills</h2>
     <h2 className={styles.about}>About</h2>
 
