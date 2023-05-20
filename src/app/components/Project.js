2abfa752e9d3aecbee27943406c29cfce2ae9e08
“sgiori11” <stephaniegiori9@gmail.com>
2023-05-20 15:20:05 +0200

    built out the projects template

--- src/app/components/Project.js
+++ src/app/components/Project.js
@@ -1,9 +1,8 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import styles from '../styles/Project.module.css';
 
-export default function Project() {
+
+
+export default function Project({ project }) {
     return (
-        <h1>hello bish im a project div</h1>
+        <h1>{project.name}</h1>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,16 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import HeroSection from './components/HeroSection';
 import Sidebar from './components/Sidebar';
-import Project from './components/Project';
+import Projects from './components/Projects.js';
+import fsPromises from 'fs/promises';
+import path from 'path';
+
+
+
+
 
 export default function Home() {
+
   return (
   <div>
    <HeroSection />
@@ -12,10 +19,10 @@ export default function Home() {
     <Sidebar className={styles.sidebar} />
 
     <section className={styles.projects}>
-      <h2>projects</h2>
-      <Project />
+      <h2>Public Projects</h2>
+      <Projects />
     </section>
-    
+
     <h2 className={styles.skills}>Skills</h2>
     <h2 className={styles.about}>About</h2>
 
