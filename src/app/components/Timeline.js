e9c2bbe690d15f9dc64f482b0f1870a4ff14afd6
“sgiori11” <stephaniegiori9@gmail.com>
2023-06-03 23:31:35 +0200

    test branch

---
+++ src/app/components/Timeline.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Skills.module.css';
+import Image from 'next/image'
+
+
+export default function Timeline() {
+    return(
+        <>
+        <h1>Timeline</h1>
+        </>
+    );
+}
