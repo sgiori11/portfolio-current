d9d68af603a21c2ac43dd6137dcfa2dfb178be29
sgiori11 <stephaniegiori9@gmail.com>
2024-04-21 15:45:35 +0200

    big

--- src/app/components/Skills.js
+++ src/app/components/Skills.js
@@ -3,19 +3,19 @@ import styles from '../styles/Skills.module.css';
 export default function Skills() {
   return (
     <section className={styles.grid}>
+       <div>
+        <p>Tools</p>
+        {["React + Nextjs", "S(CSS)", "Git", "Figma", "Webflow", "Optimizely", "Google Analytics", "Midjourney"].map((tool, index) => (
+          <span key={index}>
+            {tool}
+          </span>
+        ))}
+      </div>
       <div>
         <p>UX Research Methods</p>
         {["A/B Testing", "Data Analysis", "Heuristic Evaluations", "Voice of Customer Surveys", "Screen Recordings"].map((skill, index) => (
           <span key={index}>
             {skill}
-          </span>
-        ))}
-      </div>
-      <div>
-        <p>Tools</p>
-        {["React + Nextjs", "S(CSS)", "Git", "Figma", "Webflow", "Optimizely", "Google Analytics", "Midjourney"].map((tool, index) => (
-          <span key={index}>
-            {tool}
           </span>
         ))}
       </div>
